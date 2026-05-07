5db057e636fa571bf3698a1e82f0767f6623ed02
Marco Arnez Torrico <marcoat05.dev@gmail.com>
2026-05-06 22:46:01 -0400

    Mejora del prompt para mayor flexibilidad v4

--- ChatConIA/procesador.py
+++ ChatConIA/procesador.py
@@ -38,7 +38,7 @@ class Procesador:
         if instruccion is None:
             return "Porfavor usa correctamente el formato -> 'INSTRUCCION : Texto'"
 
-        promptFinal = f"Actúa según esta instrucción: {instruccion}. Sobre el siguiente tema o texto: {texto}. Responde de forma clara, directa y sin saludos."
+        promptFinal = "Actúa según esta instrucción: " + instruccion + "Sobre el siguiente tema o texto: " + texto + ".Responde de forma clara, directa y sin saludos."
         
         respuesta = self.chat_session.send_message(promptFinal)
         respuestaFinal = respuesta.text
